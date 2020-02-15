544b2aba4b303fc358b391a97e786aa0e810c3aa
Rukmoni <rukmoni.be@gmail.com>
2020-02-15 16:33:40 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello From React !</h1>
+     
+    </div>
+  );
+}
